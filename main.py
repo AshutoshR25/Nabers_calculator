86b8b921312fc15dfbc2f74a1380ef36eaa4e450
Ashutosh Rajput <ashutoshrajput1425@gmail.com>
2024-10-14 18:58:46 +1100

    main.py has new clean code

--- main.py
+++ main.py
@@ -1,34 +1,133 @@
+#!/usr/bin/env python3
 import pandas as pd
-from functions import NabersPredictor
-
+import json
 
 def main():
-    predictor = NabersPredictor()
 
-    # Load data and train/test split
-    X_train, X_test, y_train, y_test = predictor.load_data()
+    idx = pd.IndexSlice
+    #alist = [1000, 2000, 3000, 4000, 5000]
+    hlist = [40, 50, 60]
 
-    # Evaluate models and find the best one
-    predictor.evaluate_models(X_train, X_test, y_train, y_test)
+    #df_scope2 = pd.read_excel('data/Scopes.xlsx', sheet_name='scope2_emissions')
+    #df_scope3 = pd.read_excel('data/Scopes.xlsx', sheet_name='scope3_emissions')
+    
+    with open('data/Scopes.json') as file:
+        data = json.load(file)
+    
+    df_scope2 = pd.DataFrame(data['scope2_emissions'])
+    df_scope3 = pd.DataFrame(data['scope3_emissions'])
 
-    # Train the best model on the full dataset
-    X, y = pd.concat([X_train, X_test]), pd.concat([y_train, y_test])
-    predictor.train_best_model(X, y)
+    # Set 'state' as the index
+    df_scope2.set_index('state', inplace=True)
+    df_scope3.set_index('state', inplace=True)
 
-    while True:
-        # Take inputs from the user
-        floor_area = int(input("Enter the floor area (m2): "))
-        hours = int(input("Enter the hours: "))
-        electricity = float(input("Enter the electricity (kWh): "))
+    print(df_scope2)
+    print(df_scope3)
 
-        # Predict the star rating
-        predicted_star_rating = predictor.predict_star_rating(floor_area, hours, electricity)
-        print(f"The predicted Star Rating is: {predicted_star_rating:.1f}")
+    # Get user input
+    area = int(input("Enter the area: "))
+    hours = int(input("Enter the hours: "))
+    elec = int(input("Enter the electricity consumption (kWh): "))
+    gas = int(input("Enter the gas consumption: "))
+    diesel = int(input("Enter the diesel consumption: "))
+    state = input("Enter the state:")
+    year = int(input("Enter the year:"))
 
-        choice = input("Do you want to continue? (y/n) ")
-        if choice.lower() != 'y':
-            break
+    if year >= 2020 and year <= 2024:
+        year = 2020
+
+    #value = df_scope2.loc[state, int(year)]
+    scope2_val = df_scope2.at[str(state), str(year)]
+    scope3_val = df_scope3.at[str(state), str(year)]
+
+    scope2_elec = float(scope2_val)
+    scope3_elec = float(scope3_val)
+
+    scope1_gas = data['scope1_gas'][0]['value']
+    scope1_diesel = data['scope1_diesel'][0]['value']
+
+    emissions = (elec*(scope2_elec+scope3_elec)) + (gas*scope1_gas) + (diesel*scope1_diesel)
+
+    df1 = pd.read_excel('data/Nabers_3_Star.xlsx', sheet_name='Sheet1')
+    df2 = pd.read_excel('data/Nabers_4_Star.xlsx', sheet_name='Sheet1')
+    df3 = pd.read_excel('data/Nabers_5_Star.xlsx', sheet_name='Sheet1')
+    df = pd.concat([df1, df2, df3])
+    df.columns = ['area', 'hours', 'emissions', 'stars']
+    df.set_index(['stars', 'area', 'hours'], inplace=True)
+    df = df.unstack(['stars'])
+
+
+    # get normalised emissionstricity allowance
+    df1 = df.loc[idx[1000, :], :]
+    df1 = df1 / 1000
+
+    #print(df1)
+    df1.index = df1.index.droplevel()
+
+    # get the hours fraction
+    h1 = next((i for i in hlist if i > hours), None)
+    i = hlist.index(h1)
+    print(h1)
+    h0 = hlist[i - 1]
+    print(h0)
+    f = ((hours - h0) / (h1 - h0))
+    print(f)
+
+    # find the benchmark values for each star rating
+    dfa = df1.loc[h0]
+    dfb = df1.loc[h1]
+    dfc = dfa + (dfb - dfa) * f
+    dfc.index = dfc.index.droplevel()
+    dfc = dfc[::-1]
+    print(dfc)
+
+    dfc = pd.DataFrame(list(dfc.items()), columns=['stars', 'emissions']).set_index('stars')
+    dfc['emissions'] = dfc['emissions'].round(2)
+
+    #print(dfc)
+
+    # get normalised emissions actual
+    x = emissions / area
+
+    smaller_values = dfc[dfc['emissions'] <= x]
+    larger_values = dfc[dfc['emissions'] >= x]
+    closest_smaller_row = smaller_values.iloc[-1] if not smaller_values.empty else None
+    closest_larger_row = larger_values.iloc[0] if not larger_values.empty else None
+
+    # Will remove this if condition once we have 1 star 2 star and 6 star data.
+    if closest_smaller_row is not None:
+        closest_smaller_value = closest_smaller_row['emissions']
+        smaller_star_rating = closest_smaller_row.name 
+    else:
+        closest_smaller_value = None
+        smaller_star_rating = None
+
+    if closest_larger_row is not None:
+        closest_larger_value = closest_larger_row['emissions']
+        larger_star_rating = closest_larger_row.name  
+    else:
+        closest_larger_value = None
+        larger_star_rating = None
+
+    if closest_smaller_value is not None and closest_larger_value is not None:
+        x1, y1 = closest_smaller_value, smaller_star_rating
+        x2, y2 = closest_larger_value, larger_star_rating
+
+        if y2 == y1:
+            m = 0
+        else:
+            m = (y2 - y1) / (x2 - x1)  # Slope
+        
+        c = y1 - m * x1            # constant
+
+        interpolated_star_rating = m * x + c # formula y = mx +c
+
+        print(f"Closest smaller value: {closest_smaller_value} with star rating {smaller_star_rating}")
+        print(f"Closest larger value: {closest_larger_value} with star rating {larger_star_rating}")
+        print(f"The interpolated star rating for emissions {emissions} is: {interpolated_star_rating}")
+    else:
+        print("Could not find suitable values for interpolation.")
+
 
 if __name__ == '__main__':
     main()
-
