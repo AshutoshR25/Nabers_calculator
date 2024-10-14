d0bacadd8ad759db8c20d28a7a1810bafdd2a133
Ashutosh Rajput <ashutoshrajput1425@gmail.com>
2024-10-14 13:48:32 +1100

    All 4 years scope emissions added.

--- testing.py
+++ testing.py
@@ -5,24 +5,59 @@ import pandas as pd
 def main():
 
     idx = pd.IndexSlice
-    alist = [1000, 2000, 3000, 4000, 5000]
+    #alist = [1000, 2000, 3000, 4000, 5000]
     hlist = [40, 50, 60]
 
-    area = 3800
-    hours = 38
-    elec = 275700
+    df_scope2 = pd.read_excel('data/Scopes.xlsx', sheet_name='scope2_emissions')
+    df_scope3 = pd.read_excel('data/Scopes.xlsx', sheet_name='scope3_emissions')
+
+    # Set 'state' as the index
+    df_scope2.set_index('state', inplace=True)
+    df_scope3.set_index('state', inplace=True)
+    
+    # area = 3800
+    # hours = 38
+    # elec = 261700 #kWH   #206743 kg CO2-e
+    # gas = 0
+    # diesel = 0
+
+    # Get user input
+    area = int(input("Enter the area: "))
+    hours = int(input("Enter the hours: "))
+    elec = int(input("Enter the electricity consumption (kWh): "))
+    gas = int(input("Enter the gas consumption: "))
+    diesel = int(input("Enter the diesel consumption: "))
+    state = input("Enter the state:")
+    year = int(input("Enter the year:"))
+
+    #value = df_scope2.loc[state, int(year)]
+    scope2_val = df_scope2.at[str(state), int(year)]
+    scope3_val = df_scope3.at[str(state), int(year)]
+
+    scope2_elec = float(scope2_val)
+    scope3_elec = float(scope3_val)
+    print(scope2_elec)
+    print(scope3_elec)
+
+    scope1_gas = 0.05153
+    scope1_diesel = 0.0702
+
+    #emissions = (elec*1.09) + (gas*0.05)
+    #emissions = (elec*(1.09)) + (gas*scope1_gas) + (diesel*scope1_diesel)
+    emissions = (elec*(scope2_elec+scope3_elec)) + (gas*scope1_gas) + (diesel*scope1_diesel)
+    print(emissions)
 
     df1 = pd.read_excel('data/Nabers_3_Star.xlsx', sheet_name='Sheet1')
     df2 = pd.read_excel('data/Nabers_4_Star.xlsx', sheet_name='Sheet1')
     df3 = pd.read_excel('data/Nabers_5_Star.xlsx', sheet_name='Sheet1')
     df = pd.concat([df1, df2, df3])
-    df.columns = ['area', 'hours', 'elec', 'stars']
+    df.columns = ['area', 'hours', 'emissions', 'stars']
     df.set_index(['stars', 'area', 'hours'], inplace=True)
     df = df.unstack(['stars'])
 
-    #print(df)
+    print(df)
 
-    # get normalised electricity allowance
+    # get normalised emissionstricity allowance
     df1 = df.loc[idx[1000, :], :]
     df1 = df1 / 1000
 
@@ -48,30 +83,30 @@ def main():
     dfc = dfc[::-1]
     print(dfc)
 
-    dfc = pd.DataFrame(list(dfc.items()), columns=['stars', 'elec']).set_index('stars')
-    dfc['elec'] = dfc['elec'].round(2)
+    dfc = pd.DataFrame(list(dfc.items()), columns=['stars', 'emissions']).set_index('stars')
+    dfc['emissions'] = dfc['emissions'].round(2)
 
     print(dfc)
 
-    # get normalised elec actual
-    x = elec / area
+    # get normalised emissions actual
+    x = emissions / area
     print(x)
 
-    smaller_values = dfc[dfc['elec'] <= x]
-    larger_values = dfc[dfc['elec'] >= x]
+    smaller_values = dfc[dfc['emissions'] <= x]
+    larger_values = dfc[dfc['emissions'] >= x]
     closest_smaller_row = smaller_values.iloc[-1] if not smaller_values.empty else None
     closest_larger_row = larger_values.iloc[0] if not larger_values.empty else None
 
     # Will remove this if condition once we have 1 star 2 star and 6 star data.
     if closest_smaller_row is not None:
-        closest_smaller_value = closest_smaller_row['elec']
+        closest_smaller_value = closest_smaller_row['emissions']
         smaller_star_rating = closest_smaller_row.name 
     else:
         closest_smaller_value = None
         smaller_star_rating = None
 
     if closest_larger_row is not None:
-        closest_larger_value = closest_larger_row['elec']
+        closest_larger_value = closest_larger_row['emissions']
         larger_star_rating = closest_larger_row.name  
     else:
         closest_larger_value = None
@@ -92,74 +127,10 @@ def main():
 
         print(f"Closest smaller value: {closest_smaller_value} with star rating {smaller_star_rating}")
         print(f"Closest larger value: {closest_larger_value} with star rating {larger_star_rating}")
-        print(f"The interpolated star rating for electricity {elec} is: {interpolated_star_rating}")
+        print(f"The interpolated star rating for emissions {emissions} is: {interpolated_star_rating}")
     else:
         print("Could not find suitable values for interpolation.")
 
 
-
-# Jon has Commented code
-# def main():
-
-#     df1 = pd.read_excel('data/Nabers_3_Star.xlsx', sheet_name='Sheet1')
-#     df2 = pd.read_excel('data/Nabers_4_Star.xlsx', sheet_name='Sheet1')
-#     df3 = pd.read_excel('data/Nabers_5_Star.xlsx', sheet_name='Sheet1')
-#     df = pd.concat([df1, df2, df3])
-#     df.columns = ['area', 'hours', 'elec', 'stars']
-#     df.set_index(['stars', 'area', 'hours'], inplace=True)
-#     df = df.unstack('stars')
-#     df.columns = df.columns.droplevel()
-
-
-
-#     # df.reset_index(inplace=True)
-#     alist = [1000, 2000, 3000, 4000, 5000]
-#     hlist = [40, 50, 60]
-
-#     area = 4600
-#     hours = 45
-#     elec = 450000
-
-#     a1 = next((i for i in alist if i > area), None)
-#     i = alist.index(a1)
-#     a0 = alist[i - 1]
-
-#     h1 = next((i for i in hlist if i > hours), None)
-#     i = hlist.index(h1)
-#     h0 = hlist[i - 1]
-
-#     print('--------------')
-#     print(df)
-#     # filter by area / hours
-#     idx = pd.IndexSlice
-#     df1 = df.loc[idx[a0, h0:h1], :]
-#     df2 = df.loc[idx[a1, h0:h1], :]
-
-#     # filter by hours
-#     # df1 = df1.loc[idx[a0, :], :]
-#     # df2 = df2.loc[idx[a1, :], :]
-
-
-
-#     print('--------------')
-#     print(df1)
-#     print('--------------')
-#     print(df2)
-
-#     # print('--------------')
-#     # print('--------------')
-#     # print(df1 - elec)
-#     # print('--------------')
-#     # print(df2 - elec)
-
-#     # f = ((area - a0) / (a1 - a0))
-#     # print(f)
-#     # dfx = df1 + df2 * f
-#     # print(dfx)
-
-#     # df = df[df['area'] < a]
-
-
 if __name__ == '__main__':
     main()
-
